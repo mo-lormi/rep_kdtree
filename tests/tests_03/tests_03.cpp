b2a65618ce4909de631016d2789fbfc1744fe3af
Lorenzo Milazzo <lorenzo.milazzo@metoffice.gov.uk>
2026-06-29 11:42:39 +0100

    added test

--- tests/tests_03/tests_03.cpp
+++ tests/tests_03/tests_03.cpp
@@ -1,6 +1,6 @@
 
 //
-// last modified: 11.06.26
+// last modified: 29.06.26
 //
 
 //--
@@ -18,8 +18,8 @@
 
 
 void generatePointCloudGrid(PointCloud<float>& pc,
-			    const std::size_t& no_lon,
-			    const std::size_t& no_lat) {
+                            const std::size_t& no_lon,
+                            const std::size_t& no_lat) {
 
   const double lon_min = 0.0;
   const double lon_max = 360.0;
@@ -105,9 +105,72 @@ void test_01() {
   std::cout << "nearest neighbors [index; Long/Lat; squared distance]: " << std::endl;
   for (size_t i = 0; i < no_nneighbors; i++) {
     std::cout << idxs_nn[i] << "; "
-	      << grid.pts[idxs_nn[i]].x << ", "
-	      << grid.pts[idxs_nn[i]].y << "; "
-	      << dists_squared[i] << std::endl;
+              << grid.pts[idxs_nn[i]].x << ", "
+              << grid.pts[idxs_nn[i]].y << "; "
+              << dists_squared[i] << std::endl;
+  }
+
+  std::cout << "\n... done\n" << std::endl;
+  auto end = std::chrono::steady_clock::now();
+  auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  std::cout << "elapsed time: " << elapsed.count() << " [ms]\n" << std::endl;
+
+}
+
+
+void test_02() {
+
+  std::cout << "\n> test #02\n\nstarting ...\n" << std::endl;
+  auto start = std::chrono::steady_clock::now();
+
+  // dimensionality
+  const int dim{2};
+  
+  // numbers of lon/lat
+  const std::size_t no_lon {1280};
+  const std::size_t no_lat {960};
+
+  PointCloud<float> grid;
+
+  // generating cloud points ...
+  generatePointCloudGrid(grid, no_lon, no_lat);
+
+  //displayPointCloud(grid, (no_lon * no_lat));
+
+  using kdtree_index = nanoflann::KDTreeSingleIndexAdaptor<
+    nanoflann::L2_Simple_Adaptor<float, PointCloud<float>>,
+    PointCloud<float>,
+    dim,
+    uint32_t>;
+
+  kdtree_index index(dim, grid, {10});
+  
+  // query point (reference point) in 2D space
+  const float query_point[dim] = {33.75, -9.45};
+  
+  // search radius
+  const float search_radius {0.1};
+
+  // carrying out a search within a ROI (within a search radius) ...
+
+  // indeces of the points within the ROI and squared distances
+  std::vector<nanoflann::ResultItem<uint32_t, float>> roi_specs;
+
+  const size_t no_roi_points = index.radiusSearch(&query_point[0],
+                                                  search_radius,
+                                                  roi_specs);
+  
+        
+  std::cout << "\n++ search within a ROI" << std::endl;
+  std::cout << "query point (reference point): "
+            << query_point[0] << ", " << query_point[1]
+            << "\nno. of ROI points = " << no_roi_points << std::endl;
+  std::cout << "ROI points [index; Long/Lat; squared distance]: " << std::endl;
+  for (size_t i = 0; i < no_roi_points; i++) {
+    std::cout << roi_specs[i].first << "; "
+              << grid.pts[roi_specs[i].first].x << ", "
+              << grid.pts[roi_specs[i].first].y << "; "
+              << roi_specs[i].second << std::endl;
   }
 
   std::cout << "\n... done\n" << std::endl;
@@ -125,6 +188,7 @@ int main(void)
 
   // battery of tests
   test_01();
+  test_02();
   
   return(0);
 }
